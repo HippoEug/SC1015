3b702fa12ad0bb586c7344628e435c12c680e654
Eugene <efoo10@gmail.com>
2022-01-19 20:07:25 +0800

    Update P1P2.py

--- Lab1/P1P2.py
+++ Lab1/P1P2.py
@@ -6,20 +6,26 @@ import pandas as pd # Library for Data Acquisition and Preparation
 ### (a) Importing "train.csv" Data
 # train_data = pd.read_csv('train.csv', header = None)
 train_data = pd.read_csv('train.csv')
-train_data.head()
+# train_data.head()
+train_data
 
 ### (b) Checking number of Rows & Variables
 print("Data Shape: ", train_data.shape)
+print("Number of Rows: ", len(train_data))
+print("Number of Columns: ", len(train_data.columns))
 
 ### (c) Checking Data Types (Numeric/Categorical)
-print(train_data.dtypes)
+# print(train_data.dtypes)
+train_data.dtypes
 print(train_data.columns)
 
 ### (d) .info() Method
-print(train_data.info())
+# print(train_data.info())
+train_data.info()
 
 ### (e) .describe() Method
-print(train_data.describe())
+# print(train_data.describe())
+train_data.describe()
 
 # Problem 2
 ### Importing Essential Libraries
@@ -27,6 +33,7 @@ import numpy as np # Library for Numeric Computations in Python
 import pandas as pd # Library for Data Acquisition and Preparation
 import seaborn as sb # Higher-level library for Data Visualization
 # import matplotlib as mp # Low-level library for Data Visualization
+
 import matplotlib.pyplot as plt # we only need pyplot
 # sb.set() # set the default Seaborn style for graphics
 
@@ -41,8 +48,9 @@ print("Data Number of Tables: ", len(olympic_data))
 olympic_data[2].head()
 
 ### (d) Extracting Medal Table as New Pandas DataFrame
-medal_table = pd.DataFrame(olympic_data[2])
-medal_table.head()
+medal_table = pd.DataFrame(olympic_data[2][:86])
+# medal_table.head()
+medal_table
 
 ### (e) Extracting Top 20 Countries from Medal Table as New DataFrame
 top_countries = pd.DataFrame(medal_table['NOC'])
